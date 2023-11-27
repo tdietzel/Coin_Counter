873c3c82db02cab615c6d4b43cde4a27130da82b
mejia-dev <aaron.mejia8484@gmail.com>
2023-11-27 10:30:48 -0800

    Pass third test for CoinCounter
    Co-authored-by: Trent Dietzel <tdietzel@users.noreply.github.com>

--- src/services/coinCounter.js
+++ src/services/coinCounter.js
@@ -1,20 +1,23 @@
 export const checkInput = (input) => {
-    if (isNaN(input)) {
-      return "error";
-    }
-    const calculateCoins = (total,array) => {
-      if (total >= .25) {
-        //do quarter conversion 
-        const newTotal = total - 0.25;
-        const newArray = [array[0] + 1, array[1], array[2], array[3]];
-        return calculateCoins(newTotal,newArray);
-      } else {
-        return array;
-      }
-    }
-    return calculateCoins(input,[0,0,0,0]);
+  if (isNaN(input)) {
+    return "error";
   }
-
+  const calculateCoins = (inputTotal, array) => {
+    const total = Math.round(inputTotal * 100) / 100;
+    if (total >= .25) {
+      const newTotal = total - 0.25;
+      const newArray = [array[0] + 1, array[1], array[2], array[3]];
+      return calculateCoins(newTotal, newArray);
+    } else if ((total >= .10) && (total < .25)) {
+      const newTotal = total - 0.10;
+      const newArray = [array[0], array[1] + 1, array[2], array[3]];
+      return calculateCoins(newTotal, newArray);
+    } else {
+      return array;
+    }
+  }
+  return calculateCoins(input, [0, 0, 0, 0]);
+}
 
 // if (input >= .25) {
 //   //do quarter conversion
@@ -46,3 +49,6 @@ export const checkInput = (input) => {
 //     return incrementCounter(counter + 1);
 //   }
 // }
+
+
+

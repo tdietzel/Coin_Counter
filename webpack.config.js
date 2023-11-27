e01c03b5627e6024078ca064d13b7be14f64546b
mejia-dev <aaron.mejia8484@gmail.com>
2023-11-27 08:40:23 -0800

    Initial commit
    Co-authored-by: Trent Dietzel <tdietzel@users.noreply.github.com>

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
       verbose: true
     }),
     new HtmlWebpackPlugin({
-      title: 'Shape Tracker',
+      title: 'Coin Counter',
       template: './src/index.html',
       inject: 'body'
     })

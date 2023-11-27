d49bc295010ef1917b088483cb3fca41bacedf13
mejia-dev <aaron.mejia8484@gmail.com>
2023-11-27 10:05:48 -0800

    Write third test for CoinCounter
    Co-authored-by: Trent Dietzel <tdietzel@users.noreply.github.com>

--- __tests__/coinCounter.test.js
+++ __tests__/coinCounter.test.js
@@ -11,4 +11,9 @@ describe ('checkInput', () => {
     expect(checkInput(.50)).toEqual([2,0,0,0]);
   });
 
+  test("It should add 1 to the dimes value if input is greater than or equal to .10", () => {
+    expect(checkInput(.10)).toEqual([0,1,0,0]);
+    expect(checkInput(.35)).toEqual([1,1,0,0]);
+  });
+
 })

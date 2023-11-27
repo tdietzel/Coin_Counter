ad717fc4a9f4917b91406334b1464a978e031596
mejia-dev <aaron.mejia8484@gmail.com>
2023-11-27 09:30:05 -0800

    Write second test for coinCounter
    Co-authored-by: Trent Dietzel <tdietzel@users.noreply.github.com>

--- __tests__/coinCounter.test.js
+++ __tests__/coinCounter.test.js
@@ -6,4 +6,8 @@ describe ('CoinCounter', () => {
     expect(CoinCounter.checkInput("text")).toEqual("error");
   });
 
+  test("It should add 1 to the quarters value if input is greater than or equal to .25", () => {
+    expect(CoinCounter.checkInput(.25)).toEqual([1,0,0,0]);
+  });
+
 })

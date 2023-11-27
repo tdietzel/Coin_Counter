1263c6b02a9a1660b8c42231cfd7a06846aab004
mejia-dev <aaron.mejia8484@gmail.com>
2023-11-27 10:04:25 -0800

    Pass second test for CoinCounter
    Co-authored-by: Trent Dietzel <tdietzel@users.noreply.github.com>

--- __tests__/coinCounter.test.js
+++ __tests__/coinCounter.test.js
@@ -1,13 +1,14 @@
-import { CoinCounter } from '../src/services/coinCounter.js'
+import { checkInput } from '../src/services/coinCounter.js'
 
-describe ('CoinCounter', () => {
+describe ('checkInput', () => {
 
   test("It should return an error if input is NaN", () => {
-    expect(CoinCounter.checkInput("text")).toEqual("error");
+    expect(checkInput("text")).toEqual("error");
   });
 
   test("It should add 1 to the quarters value if input is greater than or equal to .25", () => {
-    expect(CoinCounter.checkInput(.25)).toEqual([1,0,0,0]);
+    expect(checkInput(.25)).toEqual([1,0,0,0]);
+    expect(checkInput(.50)).toEqual([2,0,0,0]);
   });
 
 })
--- src/services/coinCounter.js
+++ src/services/coinCounter.js
@@ -1,8 +1,48 @@
-export class CoinCounter {
-
-  static checkInput(input) {
+export const checkInput = (input) => {
     if (isNaN(input)) {
       return "error";
     }
+    const calculateCoins = (total,array) => {
+      if (total >= .25) {
+        //do quarter conversion 
+        const newTotal = total - 0.25;
+        const newArray = [array[0] + 1, array[1], array[2], array[3]];
+        return calculateCoins(newTotal,newArray);
+      } else {
+        return array;
+      }
+    }
+    return calculateCoins(input,[0,0,0,0]);
   }
-}
+
+
+// if (input >= .25) {
+//   //do quarter conversion
+// } else if (input >= .10) {
+
+// } else if (input >= .05) {
+
+// } else if (input >= .01) {
+
+// } else {
+
+// }
+
+// From a functional approach, we'd need to do something different:
+
+// const x = 1
+// const newX = 2 + x
+
+
+// const incrementCounter = (counter) => {
+//   // This is the termination condition.
+//   if (isNaN(counter)) {
+//     return;
+//   }
+//   if (counter >= 3) {
+//     return counter;
+//   } else {
+//     console.log(counter);
+//     return incrementCounter(counter + 1);
+//   }
+// }
